d1a8dfef2d555bb685488c8b4dfc6be7e169f59f
henry-n2 <ujjalbhattacharya525@gmail.com>
2025-04-05 05:11:59 +0530

    Settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!bp990y8wp4pt^hm^5%$68jvn@9+@9^_z8(7e+2=lyob9fz!z%'
-
+# SECRET_KEY = 'django-insecure-!bp990y8wp4pt^hm^5%$68jvn@9+@9^_z8(7e+2=lyob9fz!z%'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
     'products.apps.ProductsConfig',
     'AdminPanel',
     'Vendors',
-    'ckeditor',
+    'django_ckeditor_5',
     'payments',
     'prediction',
     'about'
@@ -93,9 +93,17 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ecogridai',
+        'USER': 'ecogridai_user',
+        'PASSWORD': 'w69V3yaVbyYpaPQ3R45ctQLoJQ5tRSIE',
+        'HOST': 'dpg-cvo6aimmcj7s73fucs60-a',  # <- This is the problem
+        'PORT': '5432',
     }
 }
 
+# url="postgresql://ecogridai_user:w69V3yaVbyYpaPQ3R45ctQLoJQ5tRSIE@dpg-cvo6aimmcj7s73fucs60-a/ecogridai"
+DATABASES["default"] = dj_database_url.parse("postgresql://ecogridai_user:w69V3yaVbyYpaPQ3R45ctQLoJQ5tRSIE@dpg-cvo6aimmcj7s73fucs60-a.oregon-postgres.render.com/ecogridai")
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
